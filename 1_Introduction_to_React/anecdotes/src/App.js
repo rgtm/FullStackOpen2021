0c0fc1d87fccd494a76312a6348fc2491fa8c52e
Ross McIlvanney <rossgtm@me.com>
2021-09-07 17:45:22 +0100

    Exercise 1.5 completed

--- 1_Introduction_to_React/anecdotes/src/App.js
+++ 1_Introduction_to_React/anecdotes/src/App.js
@@ -2,37 +2,40 @@ import React from 'react'
 
 const App = () => {
   // Variable declarations
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   // Returned JSX for the overall app
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
 // Components
 const Header = (props) => {
+  console.log(props);
   return (
     <h1>
-      {props.course}
+      {props.course.name}
     </h1>
   )
 }
@@ -56,7 +59,6 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props)
   return (
     <p>
       Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
